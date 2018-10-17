a51f4bd1da0bfba49c418a56db9f64f5cbd6a82d
hanianowak <hania.nowak2018@gmail.com>
2018-10-17 02:12:37 -0500

    jdbc - exercise

---
+++ src/main/java/MySQLAdsDao.java
@@ -0,0 +1,57 @@
+import com.mysql.cj.jdbc.Driver;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MySQLAdsDao implements Ads {
+    private Connection connection;
+
+    public MySQLAdsDao(Config config) {
+        try {
+            DriverManager.registerDriver(new Driver());
+            connection = DriverManager.getConnection(
+                    config.getUrl(),
+                    config.getUser(),
+                    config.getPassword()
+            );
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    @Override
+    public List<Ad> all() {
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery("SELECT * FROM ads");
+
+            ArrayList<Ad> ads = new ArrayList<>();
+            while (rs.next()) {
+                Ad ad = new Ad(rs.getLong("id"), rs.getLong("user_id"), rs.getString("title"), rs.getString("description"));
+                ads.add(ad);
+            }
+            return ads;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    public Long insert(Ad ad) {
+        try {
+            Statement statement = connection.createStatement();
+            String sql = "insert into ads(user_id, title, description) values (" + ad.getId() + ", '" + ad.getTitle() + "', '" + ad.getDescription() + "')";
+
+            statement.executeUpdate(sql, Statement.RETURN_GENERATED_KEYS);
+            ResultSet rs = statement.getGeneratedKeys();
+            if (rs.next()) {
+                return rs.getLong(1);
+            }
+            throw new RuntimeException("something went wrong!");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/main/java/base.sql
@@ -0,0 +1,23 @@
+create database adlister_db;
+
+create table users (
+id int unsigned not null auto_increment,
+username varchar(50),
+email varchar(50),
+password varchar(50),
+primary key (id)
+);
+
+create table ads (
+id int unsigned not null auto_increment,
+user_id int unsigned default null,
+title varchar(250),
+description varchar(250),
+primary key (id),
+foreign key (user_id) references users (id)
+);
+
+insert into users (username, email, password)
+values ("hania", "h@jsd.com", "jsdssda"),
+("tom", "sas@jdsf.com", "jdkajsdas"),
+("peggy", "p@skdfs.com","ksjdskdf");
